f3bb3e80cc1bdefcd1add1cc6d3d093c0dc97e76
ESX <759792402@qq.com>
2017-02-25 15:02:19 +0800

    打飞机Demo

---
+++ 打飞机/Plane.swift
@@ -0,0 +1,29 @@
+//
+//  Plane.swift
+//  打飞机
+//
+//  Created by Ying on 16/9/22.
+//  Copyright © 2016年 liying. All rights reserved.
+//
+
+import UIKit
+
+class Plane: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
